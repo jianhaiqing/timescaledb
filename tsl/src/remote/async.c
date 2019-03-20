5e2a80a2046e058956e79c075e314e28d4632da0
niksa <niksa@timescale.com>
2019-03-20 17:05:48 +0100

    Add support for binary response
    We request binary responses from data nodes only if all requested
    columns have binary IN function, otherwise we fall back to text
    format. We use binary only when fetching query results or when getting
    data from RETURNING statements. Maybe in the future we'd like to
    change all the communication code to use binary. However, it seems
    that it would not bring significant performance benefit so it was left
    out for now.

--- tsl/src/remote/async.c
+++ tsl/src/remote/async.c
@@ -81,7 +81,7 @@ static AsyncRequest *
 async_request_send_with_params_formats_elevel(PGconn *conn, const char *sql, int n_params,
 											  const char *const *param_values,
 											  const int *param_lengths, const int *param_formats,
-											  int elevel)
+											  int elevel, int res_format)
 {
 	AsyncRequest *req = palloc(sizeof(AsyncRequest));
 
@@ -112,7 +112,7 @@ async_request_send_with_params_formats_elevel(PGconn *conn, const char *sql, int
 							   param_values,
 							   param_lengths,
 							   param_formats,
-							   /* result format - text */ 0))
+							   res_format))
 	{
 		/*
 		 * null is fine to pass down as the res, the connection error message
@@ -126,8 +126,9 @@ async_request_send_with_params_formats_elevel(PGconn *conn, const char *sql, int
 }
 
 AsyncRequest *
-async_request_send_with_params_elevel(PGconn *conn, const char *sql, int n_params,
-									  const char *const *param_values, int elevel)
+async_request_send_with_params_elevel_res_format(PGconn *conn, const char *sql, int n_params,
+												 const char *const *param_values, int elevel,
+												 int res_format)
 {
 	return async_request_send_with_params_formats_elevel(conn,
 														 sql,
@@ -135,12 +136,14 @@ async_request_send_with_params_elevel(PGconn *conn, const char *sql, int n_param
 														 param_values,
 														 NULL,
 														 NULL,
-														 elevel);
+														 elevel,
+														 res_format);
 }
 
 AsyncRequest *
-async_request_send_with_stmt_params_elevel(PGconn *conn, const char *sql_statement,
-										   StmtParams *params, int elevel)
+async_request_send_with_stmt_params_elevel_res_format(PGconn *conn, const char *sql_statement,
+													  StmtParams *params, int elevel,
+													  int res_format)
 {
 	return async_request_send_with_params_formats_elevel(conn,
 														 sql_statement,
@@ -148,7 +151,8 @@ async_request_send_with_stmt_params_elevel(PGconn *conn, const char *sql_stateme
 														 stmt_params_values(params),
 														 stmt_params_lengths(params),
 														 stmt_params_formats(params),
-														 elevel);
+														 elevel,
+														 res_format);
 }
 
 AsyncRequest *
@@ -197,7 +201,8 @@ async_request_send_prepare(PGconn *conn, const char *sql, int n_params)
 
 static AsyncRequest *
 async_request_send_prepared_stmt_with_formats(PreparedStmt *stmt, const char *const *param_values,
-											  const int *param_lengths, const int *param_formats)
+											  const int *param_lengths, const int *param_formats,
+											  int res_format)
 {
 	AsyncRequest *req = palloc(sizeof(AsyncRequest));
 
@@ -213,7 +218,7 @@ async_request_send_prepared_stmt_with_formats(PreparedStmt *stmt, const char *co
 							 param_values,
 							 param_lengths,
 							 param_formats,
-							 0))
+							 res_format))
 	{
 		/*
 		 * null is fine to pass down as the res, the connection error message
@@ -228,16 +233,17 @@ async_request_send_prepared_stmt_with_formats(PreparedStmt *stmt, const char *co
 extern AsyncRequest *
 async_request_send_prepared_stmt(PreparedStmt *stmt, const char *const *paramValues)
 {
-	return async_request_send_prepared_stmt_with_formats(stmt, paramValues, NULL, NULL);
+	return async_request_send_prepared_stmt_with_formats(stmt, paramValues, NULL, NULL, 0);
 }
 
 AsyncRequest *
-async_request_send_prepared_stmt_with_params(PreparedStmt *stmt, StmtParams *params)
+async_request_send_prepared_stmt_with_params(PreparedStmt *stmt, StmtParams *params, int res_format)
 {
 	return async_request_send_prepared_stmt_with_formats(stmt,
 														 stmt_params_values(params),
 														 stmt_params_lengths(params),
-														 stmt_params_formats(params));
+														 stmt_params_formats(params),
+														 res_format);
 }
 
 /* Set user data. Often it is useful to attach data with a request so
