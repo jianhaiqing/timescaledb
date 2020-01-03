75fb0e7d9e9c540f812348870c2421dabe3c0bfc
Brian Rowe <brian@timescale.com>
2020-01-03 15:21:07 -0800

    Test dimension add on distributed hypertables
    Prior to this change attempting to add a dimension to a distributed
    hypertable which currently or previously contained data would fail
    with an opaque error.  This change will properly test distributed
    hypertables when adding dimensions and will print appropriate errors.

--- src/hypertable.c
+++ src/hypertable.c
@@ -1388,11 +1388,7 @@ ts_hypertable_has_tuples(Oid table_relid, LOCKMODE lockmode)
 bool
 ts_hypertable_has_chunks(Oid table_relid, LOCKMODE lockmode)
 {
-	List *chunks = find_inheritance_children(table_relid, lockmode);
-	if (chunks != NIL)
-		return true;
-	else
-		return false;
+	return find_inheritance_children(table_relid, lockmode) != NIL;
 }
 
 static void
